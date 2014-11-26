46711830837175541427f7c150e24f4ababfbfef
Erik Carlsson <info@code301.com>
2014-11-26 12:52:59 +0100

    Init.

---
+++ lib/DBIx/Class/MachinaX/SourceParser.pm
@@ -0,0 +1,39 @@
+package DBIx::Class::MachinaX::SourceParser;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+DBIx::Class::MachinaX::SourceParser - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use DBIx::Class::MachinaX::SourceParser;
+
+=head1 DESCRIPTION
+
+DBIx::Class::MachinaX::SourceParser is
+
+=head1 AUTHOR
+
+Erik Carlsson E<lt>info@code301.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- Erik Carlsson
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
---
+++ t/basic.t
@@ -0,0 +1,8 @@
+use strict;
+use Test::More;
+use DBIx::Class::MachinaX::SourceParser;
+
+# replace with the actual test
+ok 1;
+
+done_testing;
